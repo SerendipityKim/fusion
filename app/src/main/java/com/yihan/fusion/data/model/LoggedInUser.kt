717452c7b832a52e8af008e7870b2a6c805c7753
yihan <yangzixuan@shangyoo.com>
2020-06-29 20:32:00 +0800

    create project

---
+++ app/src/main/java/com/yihan/fusion/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.yihan.fusion.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
